2119b11d17381c02492eee7be9b34e390d9fb261
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-05-14 18:11:20 -0500

    Met MVP all tests passing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 import './App.css';
+import Character from "./components/Character.js"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+    .get('https://swapi.dev/api/people')
+    .then(response => {
+      console.log("HERE IS THE DATA:", response.data)  // array of character objects
+
+      setCharacters(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
 
   return (
     <div className="App">
+
       <h1 className="Header">Characters</h1>
+
+      {console.log("HERE IS STATE:", characters)}
+
+      {characters.map((charObj) => {
+        return <Character name={charObj.name} key={charObj.name} />
+      })}
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
 // Write your Character component here
+import React from 'react'
+import Styled from 'styled-components'
+
+const StyledCharacter = Styled.div`
+.character {
+  padding: 20px;
+  border-bottom: 2px solid lightgray;
+  color: white;
+  background-color: black;
+  opacity: 0.7;
+  &:hover {
+    background-color: grey;
+    color: black;
+  }
+}
+`
+
+
+export default function Character(props) {
+  const { name } = props;
+
+  console.log("HERE ARE PROPS:", props)
+
+  return (
+    <StyledCharacter>
+      <div class="character">
+
+        <h2>{name}</h2>
+
+      </div>
+    </StyledCharacter>
+  )
+}
